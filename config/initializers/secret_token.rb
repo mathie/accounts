da3608921d0a4039c9e27e5b78303bcccd0008c3
Graeme Mathieson <mathie@woss.name>
2011-08-28 16:59:41 +0100

    rails new accounts -d mysql.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accounts::Application.config.secret_token = 'c4b63c903792e1d532753fcec6e4fa396fb7ce8e3cbf415a2f8c40090d37e5d141adc5138428a2f3a625181cc182f1b85ec30d3fa14d7aa1b193c1601af1754f'
